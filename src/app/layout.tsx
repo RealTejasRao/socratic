ae4b419c69ac14a51b6e7690c1ee4b7cfb865499
RealTejasRao <ytejas72@gmail.com>
2026-04-08 04:50:25 +0530

    fix bugs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import { ClerkProvider } from "@clerk/nextjs";
 import ClickPulse from "@/src/components/ClickPulse";
 import "./globals.css";
 
@@ -37,21 +36,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
-      <html lang="en">
-        <head>
-          <link rel="dns-prefetch" href="https://res.cloudinary.com" />
-          <link
-            rel="preconnect"
-            href="https://res.cloudinary.com"
-            crossOrigin="anonymous"
-          />
-        </head>
-        <body>
-          <ClickPulse />
-          {children}
-        </body>
-      </html>
-    </ClerkProvider>
+    <html lang="en">
+      <head>
+        <link rel="dns-prefetch" href="https://res.cloudinary.com" />
+        <link
+          rel="preconnect"
+          href="https://res.cloudinary.com"
+          crossOrigin="anonymous"
+        />
+      </head>
+      <body>
+        <ClickPulse />
+        {children}
+      </body>
+    </html>
   );
 }
